b90024302550707c1f35c5917ebf627a5a9588ee
Rodrigo Celmer <rodrigo@raks.com.br>
2021-01-26 12:25:00 -0300

    set AT+QICSGP cmd

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -52,7 +52,7 @@ const char AT_QCFG_NWSCANMODE[]	= {"AT+QCFG=\"NWSCANMODE\",0\r\n\0"};
 const char AT_QCFG_NWSCANSEQ[]	= {"AT+QCFG=\"NWSCANSEQ\",020103\r\n\0"};
 const char AT_QCFG_IOTOPMODE[]	= {"AT+QCFG=\"IOTOPMODE\",2\r\n\0"};
 const char AT_QCFG_BAND[]		= {"AT+QCFG=\"BAND\",0XF,0,0X8000004\r\n\0"};
-const char AT_QICSGP[]			= {"\r\n\0"};
+const char AT_QICSGP[]			= {"AT+QICSGP=1,1,\"virtueyes.com.br\",\"virtu\",\"virtu\",3\r\n\0"};
 const char AT_CFUN0[]			= {"AT+CFUN=0\r\n\0"};
 const char AT_CFUN1[]			= {"AT+CFUN=1\r\n\0"};
 const char AT_CREG[]			= {"AT+CREG?\r\n\0"};
