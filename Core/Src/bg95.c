018cc3814d7a8cdd11132b14e65a722d56fffd7a
Rodrigo Celmer <rodrigo@raks.com.br>
2021-02-02 14:48:04 -0300

    fix mqtt response buffer reception

--- Core/Src/bg95.c
+++ Core/Src/bg95.c
@@ -32,7 +32,7 @@ const char QMTCONN[]			= {"AT+QMTCONN=1,\"usrCelmer\",\"zgxbgfsy\",\"H7Mnnfi0_2r
 const char QMTDISC[]			= {"AT+QMTDISC=1\r\n\0"};												//resp: OK +QMTDISC: [13]1,[15]0
 const char QMTSUB[]				= {"AT+QMTSUB=1,1,\"celmer\",1\r\n\0"};									//resp: OK +QMTSUB: [12]1,[14]1,[16]0,[18]1 +QMTRECV: 1,1,"topic","msg"
 const char QMTUNS[]				= {"AT+QMTUNS=1,1,\"celmer\"\r\n\0"};									//resp: OK +QMTUNS: [12]1,[14]1,[16]0
-const char QMTPUBEX[]			= {"AT+QMTPUBEX=1,1,1,1,\"celmer\",\"life on the line\"\r\n\0"};		//resp: OK +QMTPUB: [12]1,[14]3,[16]0
+const char QMTPUBEX[]			= {"AT+QMTPUBEX=1,1,1,1,\"celmer\",\"something else\"\r\n\0"};		//resp: OK +QMTPUB: [12]1,[14]3,[16]0
 
 void bg95_sendCmd(const char *txData, uint32_t txDataSize);
 uint32_t bg95_response(eAtCmd_t cmdSent, char *response, uint32_t respTimeout);
@@ -202,18 +202,27 @@ uint32_t bg95_response(eAtCmd_t cmdSent, char *response, uint32_t respTimeout)
 			}
 		}
 
-		if(cmdSent <= AT_QNWINFO)
+		if(	(cmdSent == AT_QMTOPEN)	||
+			(cmdSent == AT_QMTCLOSE)||
+			(cmdSent == AT_QMTCONN)	||
+			(cmdSent == AT_QMTDISC)	||
+			(cmdSent == AT_QMTSUB)	||
+			(cmdSent == AT_QMTUNS)	||
+			(cmdSent == AT_QMTPUBEX))
 		{
-			if((response[bufCount-2] == 'O') && (response[bufCount-1] == 'K'))
+			if(memcmp(response, "OK +QMT", 7) == 0)
 			{
-				respACK = 1;
+				if((response[bufCount-3] == ',') && (response[bufCount-2] != '\0') && (response[bufCount-1] == ' '))
+				{
+					response[bufCount - 1] = '\0';
+					respACK = 1;
+				}
 			}
 		}
 		else
 		{
-			if((response[bufCount-1] != '\0') && (response[bufCount] == ' '))
+			if((response[bufCount-2] == 'O') && (response[bufCount-1] == 'K'))
 			{
-				response[bufCount] = '\0';
 				respACK = 1;
 			}
 		}
