ca9edef7dfc019ff4d243d5b8871cf2fbb010b0a
Rodrigo Celmer <rodrigo@raks.com.br>
2021-02-04 15:58:14 -0300

    add function pointers to radio module

--- Core/Inc/radio.h
+++ Core/Inc/radio.h
@@ -9,12 +9,71 @@ typedef enum {
 	greatSignal,
 }eRadioStatus_t;
 
-void delay_ms(uint32_t time_ms);
-void radio_reset(void);
+void delay_ms(uint32_t time_ms);	//must be on proper module
 
-void radio_turnOn(void);
-void radio_turnOff(void);
-void radio_config(void);
-void radio_connect(void);
-void radio_publish(void);	//const char *msg);
-void radio_disconnect(void);
+void radio_init(void);
+
+/**
+  * @brief  Generic radio modules
+  */
+typedef struct sRadio
+{
+	/**
+	  * @brief  Pointer to
+	  * @param	None
+	  * @retval None
+	  */
+	void (*turnOn)(void);
+
+	/**
+	  * @brief  Pointer to
+	  * @param	None
+	  * @retval None
+	  */
+	void (*turnOff)(void);
+
+	/**
+	  * @brief  Pointer to
+	  * @param	None
+	  * @retval None
+	  */
+	void (*reset)(void);
+
+	/**
+	  * @brief	Pointer to
+	  * @param	None
+	  * @retval	None
+	  */
+	void (*config)(void);
+
+	/**
+	  * @brief	Pointer to
+	  * @param	None
+	  * @retval None
+	  */
+	void (*connect)(void);
+
+	/**
+	  * @brief  Pointer to
+	  * @param	None
+	  * @retval None
+	  */
+	void (*publish)(void);	//const char *msg);
+
+	/**
+	  * @brief  Pointer to
+	  * @param	None
+	  * @retval None
+	  */
+	void (*disconnect)(void);
+}sRadio_t;
+
+sRadio_t radio;
+
+//void radio_turnOn(void);
+//void radio_turnOff(void);
+//void radio_reset(void);
+//void radio_config(void);
+//void radio_connect(void);
+//void radio_publish(void);	//const char *msg);
+//void radio_disconnect(void);
